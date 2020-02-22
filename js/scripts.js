3c13a5e8f8ceaf7f9855eadf8e73b5368cc4abcb
BA <engineervinceblair@gmail.com>
2020-02-22 17:21:07 +0300

    created jQuery to toogle between images and paragraph

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+$("#design1").click(function(){
+$("#design2").toggle();
+$("#design3").toggle();
+    
+});
+
+$("#development1").click(function(){
+$("#development2").toggle();
+$("#development3").toggle();
+       
+});
+$("#product1").click(function(){
+    $("#product2").toggle();
+    $("#product3").toggle();
+})
+});
